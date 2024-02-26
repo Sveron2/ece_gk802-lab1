4b31b2177a30ecbaf16c725f918617e0cb470cbe
Sveron2 <up1083751@upnet.gr>
2024-02-27 01:55:58 +0200

    new

--- ex0.py
+++ ex0.py
@@ -1,19 +1,33 @@
-import requests  # εισαγωγή της βιβλιοθήκης
+import requests
+from datetime import datetime
 
 
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
+def fetch_server_status(url):
+    try:
 
+        response = requests.head(url, timeout=5)
+        server_software = response.headers.get('Server', 'N/A')
+        print(f"Server Software: {server_software}")
 
-url = 'http://python.org/'  # προσδιορισμός του url
+        cookies = response.cookies
+        if cookies:
+            print("Cookies: ")
+            for cookie in cookies:
+                print(f'Name: {cookie.name}', end="\t")
 
-with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+                if cookie.expires:
+                    expires = datetime.fromtimestamp(cookie.expires).strftime('%Y-%m-%d')
+                    print(f'Expires: {expires}', end="\t")
+                print("headers: ", response.headers)
+        else:
+            print("No cookies found.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+def main():
+    url = input("Enter a URL: ")
+    fetch_server_status(url)
+
+if __name__ == "__main__":
+    main()
